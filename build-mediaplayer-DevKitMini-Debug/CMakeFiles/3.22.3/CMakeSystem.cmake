d5f8fbc2af0ed8257f9bc28c746091ee30f5f1a1
Chakshu Bhardwaj <chakshu.bhardwaj93@gmail.com>
2024-06-06 21:59:16 -0400

    Media Player App

---
+++ build-mediaplayer-DevKitMini-Debug/CMakeFiles/3.22.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.15.0-107-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.15.0-107-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/linuxdev/Qt/6.4.3/Boot2Qt/imx8mm-var-dart/toolchain/sysroots/x86_64-pokysdk-linux/usr/lib/cmake/Qt6/qt.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "armv8a")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ build-mediaplayer-DevKitMini-Debug/qtcsettings.cmake
@@ -0,0 +1,15 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/linuxdev/Qt/Examples/Qt-6.4.3/multimedia/video/build-mediaplayer-DevKitMini-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_SYSROOT" "/home/linuxdev/Qt/6.4.3/Boot2Qt/imx8mm-var-dart/toolchain/sysroots/armv8a-poky-linux" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER_TARGET" "aarch64-poky-linux" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER_TARGET" "aarch64-poky-linux" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/home/linuxdev/Qt/6.4.3/Boot2Qt/imx8mm-var-dart/toolchain/sysroots/x86_64-pokysdk-linux/usr/bin/aarch64-poky-linux/aarch64-poky-linux-g++" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/home/linuxdev/Qt/6.4.3/Boot2Qt/imx8mm-var-dart/toolchain/sysroots/x86_64-pokysdk-linux/usr/bin/aarch64-poky-linux/aarch64-poky-linux-gcc" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/linuxdev/Qt/6.4.3/Boot2Qt/imx8mm-var-dart/toolchain/sysroots/armv8a-poky-linux/usr" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/linuxdev/Qt/6.4.3/Boot2Qt/imx8mm-var-dart/toolchain/sysroots/x86_64-pokysdk-linux/usr/bin/qmake" CACHE "STRING" "" FORCE)
+set("CMAKE_TOOLCHAIN_FILE" "/home/linuxdev/Qt/6.4.3/Boot2Qt/imx8mm-var-dart/toolchain/sysroots/x86_64-pokysdk-linux/usr/lib/cmake/Qt6/qt.toolchain.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_MAKE_PROGRAM" "/home/linuxdev/Qt/6.4.3/Boot2Qt/imx8mm-var-dart/toolchain/sysroots/x86_64-pokysdk-linux/usr/bin/ninja" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
